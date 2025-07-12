df47c6d4c1592cb73ef2ad77027dd52ca8c2e584
João Lucas <jlucaso@hotmail.com>
2025-07-11 22:38:48 -0300

    chore: more cleanup

--- whatsapp-core/src/error/decryption.rs
+++
@@ -1,9 +0,0 @@
-use thiserror::Error;
-
-#[derive(Debug, Error)]
-pub enum DecryptionError {
-    #[error("event was already processed")]
-    AlreadyProcessed,
-    #[error("underlying crypto error: {0}")]
-    Crypto(#[from] anyhow::Error),
-}
