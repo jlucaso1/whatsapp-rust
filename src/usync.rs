1954c0d923fcac8ad94d39765b5665783980f114
João Lucas <jlucaso@hotmail.com>
2025-08-14 18:59:52 -0300

    refactor: streamline user device query and response handling in usync

--- src/usync.rs
+++ src/usync.rs
@@ -1,4 +1,3 @@
-use crate::binary::builder::NodeBuilder;
 use crate::binary::node::NodeContent;
 use crate::client::Client;
 use log::debug;
@@ -15,30 +14,9 @@ impl Client {
         }
 
         debug!("get_user_devices: Using normal mode for {jids:?}");
-        let mut user_nodes = Vec::new();
-        for jid in jids {
-            user_nodes.push(
-                NodeBuilder::new("user")
-                    .attr("jid", jid.to_non_ad().to_string())
-                    .build(),
-            );
-        }
 
-        let query_node = NodeBuilder::new("query")
-            .children([NodeBuilder::new("devices").attr("version", "2").build()])
-            .build();
-        let list_node = NodeBuilder::new("list").children(user_nodes).build();
         let sid = self.generate_request_id();
-        let usync_node = NodeBuilder::new("usync")
-            .attrs([
-                ("context", "message"),
-                ("index", "0"),
-                ("last", "true"),
-                ("mode", "query"),
-                ("sid", sid.as_str()),
-            ])
-            .children([query_node, list_node])
-            .build();
+        let usync_node = wacore::usync::build_get_user_devices_query(jids, sid.as_str());
 
         let iq = crate::request::InfoQuery {
             namespace: "usync",
@@ -52,27 +30,9 @@ impl Client {
 
         let resp_node = self.send_iq(iq).await?;
 
-        let list_node = resp_node
-            .get_optional_child_by_tag(&["usync", "list"])
-            .ok_or_else(|| anyhow::anyhow!("<usync> or <list> not found in usync response"))?;
+        // Delegate parsing to wacore helper
+        let devices = wacore::usync::parse_get_user_devices_response(&resp_node)?;
 
-        let mut all_devices = Vec::new();
-        for user_node in list_node.get_children_by_tag("user") {
-            let user_jid = user_node.attrs().jid("jid");
-            let device_list_node = user_node
-                .get_optional_child_by_tag(&["devices", "device-list"])
-                .ok_or_else(|| anyhow::anyhow!("<device-list> not found for user {user_jid}"))?;
-
-            for device_node in device_list_node.get_children_by_tag("device") {
-                let device_id_str = device_node.attrs().string("id");
-                let device_id: u16 = device_id_str.parse()?;
-
-                let mut device_jid = user_jid.clone();
-                device_jid.device = device_id;
-                all_devices.push(device_jid);
-            }
-        }
-
-        Ok(all_devices)
+        Ok(devices)
     }
 }
