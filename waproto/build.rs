b51e79be9d99f26131bb3c107e213308fc7c162a
João Lucas <jlucaso@hotmail.com>
2025-10-08 11:50:03 -0300

    chore: only add serde to necessary structs (dev build time)

--- waproto/build.rs
+++ waproto/build.rs
@@ -1,25 +1,27 @@
 fn main() -> std::io::Result<()> {
-    // By default, we expect the `whatsapp.rs` file to be pre-generated.
-    // This build script will only regenerate it if the `GENERATE_PROTO`
-    // environment variable is set. This is intended for developers who modify
-    // the `.proto` file.
     if std::env::var("GENERATE_PROTO").is_err() {
         println!("cargo:rerun-if-changed=build.rs");
-        // For a normal build, do nothing.
         return Ok(());
     }
-
-    // This part runs only when `GENERATE_PROTO=1` is in the environment.
     println!("cargo:rerun-if-changed=src/whatsapp.proto");
     println!("cargo:warning=GENERATE_PROTO is set, regenerating proto definitions...");
 
     let mut config = prost_build::Config::new();
-    config.type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]");
+    let serde_derive = "#[derive(serde::Serialize, serde::Deserialize)]";
 
-    // Configure prost to output the file to the `src/` directory,
-    // so it can be version-controlled.
+    config.message_attribute(".whatsapp.ADVSignedDeviceIdentity", serde_derive);
+
+    config.message_attribute(".whatsapp.RecordStructure", serde_derive);
+    config.message_attribute(".whatsapp.SessionStructure", serde_derive);
+
+    config.message_attribute(".whatsapp.PreKeyRecordStructure", serde_derive);
+    config.message_attribute(".whatsapp.SignedPreKeyRecordStructure", serde_derive);
+
+    config.message_attribute(".whatsapp.SenderKeyRecordStructure", serde_derive);
+    config.message_attribute(".whatsapp.SenderKeyStateStructure", serde_derive);
+
     config.out_dir("src/");
-
     config.compile_protos(&["src/whatsapp.proto"], &["src/"])?;
+
     Ok(())
 }
