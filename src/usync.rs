ad2c33957e5fac217378ad409161d89ec82cfcb2
João Lucas <jlucaso@hotmail.com>
2025-10-15 00:09:22 -0300

    Parallelize version fetch and transport connect in Client
    Move version resolution to Client and run it concurrently with transport
    creation for faster startup. Update Client::new to accept
    override_version and propagate it throughout. Update tests for new
    Client::new signature.

--- src/usync.rs
+++ src/usync.rs
@@ -84,6 +84,7 @@ mod tests {
             pm.clone(),
             Arc::new(crate::transport::mock::MockTransportFactory::new()),
             Arc::new(MockHttpClient),
+            None,
         )
         .await;
 
